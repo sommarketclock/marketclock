486e98eea8fb3c2451845ea3fd24bafc7fb775a1
sommarketclock <69646313+sommarketclock@users.noreply.github.com>
2020-08-14 02:44:11 +0800

    Add files via upload

---
+++ build/precache-manifest.27465e93a3a116b5b27bc215b6f45f1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f45401c6ba1c02d990e1c179c25b54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "814f4b299137edb38598",
+    "url": "/static/css/main.3be1c0cc.chunk.css"
+  },
+  {
+    "revision": "7f4b974da5d7a61c6f01",
+    "url": "/static/js/2.fdb0debb.chunk.js"
+  },
+  {
+    "revision": "0cce3cdd5176cd8ebcd9219cf9aa461c",
+    "url": "/static/js/2.fdb0debb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "814f4b299137edb38598",
+    "url": "/static/js/main.30435237.chunk.js"
+  },
+  {
+    "revision": "1ebb01a3a5d76a7ac10f",
+    "url": "/static/js/runtime-main.333e5a82.js"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/static/media/digital-7 (mono).58045dab.ttf"
+  }
+]);
